b6d7aa236b9b77a9ea0d6299c9723072c94b0606
Amit <amit653@gmail.com>
2023-10-04 23:31:46 +0530

    Update stringTobyte.go

--- stringTobyte.go
+++ stringTobyte.go
@@ -9,7 +9,19 @@ func main() {
 
 	byte := []byte(str)
 	byte[0] = 'H' // "H" will  be invalid , assignment needs a byte value
-	byte[4] = 'O' 
+	byte[4] = 'O'
 	fmt.Println(string(byte))
+
+	for _, r := range str { // range iterates over rune in string
+		fmt.Print(string(r))
+		fmt.Println()
+	}
+
+//convert a string to rune slice to access by index
+	r := []rune(str)
+	for _, i := range r {
+		fmt.Print(string(i))
+		fmt.Println()
+	}
 
 }
